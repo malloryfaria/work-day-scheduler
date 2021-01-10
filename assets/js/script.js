5f5423b9fb65a02d2d11ca01f4c1bdce4a3e751c
Mallory <mallory.faria@gmail.com>
2021-01-10 18:34:03 -0500

    javascript completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,120 @@
+// Uses Moment to get current date and current hour
 var currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
-var currentTime = moment().format("H");
-var CurrentContainer = 0;
-var tempArray = [];
+var currentHour = moment().format("H");
 
+// Declare variables
+var timeRow = $(".time-row");
+var container = $(".container");
+
+// Create an array to hold the tasks for saving to localStorage
+var tasks = [];
+
+// Function to load the page initially
+
+function loadTasks() {
+
+    timeRow.each(function(){
+        var thisRow = $(this);
+        var thisRowHr = parseInt(thisRow.attr("data-hour"));
     
-$(document).ready(function () {
-    currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
-    $("#currentDay").append(currentDate);
+        var tasksObj = {
+          hour: thisRowHr,
+          text: "",
+        }
+        tasks.push(tasksObj);
+      });
+      // Loop all rows and save to the local storage
+      localStorage.setItem("tasks", JSON.stringify(tasks)); 
 
-var planner = JSON.parse(localStorage.getItem("planner")) || initializePlanner();
-console.log(planner);
+};
 
-for (var time in planner) {
-    console.log(time, planner[time]);
-    var tr = $("<tr>")
-        // .addClass("row time-block");
-    var tdTime = $("<td>")
-        .addClass("hour") // col-md-1
-        .text(time);
-    var tdEvent = $("<td>")
-        .addClass("textArea");
+// Function to save the tasks to localStorage
 
-    var thisTime;
-    
-    if (moment(time, "h a").isSame(moment(), "hour")) {
-        thisTime = "present";
-    } else if (moment(time, "h a").isAfter(moment())) {
-        thisTime = "future";
-    } else if (moment(time, "h a").isBefore(moment())) {
-        thisTime = "past";
+function saveTasks (){
+    var hourToUpdate = $(this).parent().attr("data-hour");
+    var itemToAdd = (($(this).parent()).children("textarea")).val(); 
+    for (var j = 0; j < tasks.length; j++){
+      if (tasks[j].hour == hourToUpdate){
+       
+        tasks[j].text = itemToAdd;
+      }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    console.log("Tasks were saved");
+    renderPlanner();
+  };
+
+// Function to format the row colours depending on the current time
+
+function formatRows(){
+    timeRow.each(function(){
+    var thisRow = $(this);
+    var thisRowHr = parseInt(thisRow.attr("data-hour"));
+  
+    // Set to "Present" if current row hour is equal to this hour
+    if (thisRowHr == currentHour) {
+      thisRow.addClass("present").removeClass("past future");
     }
 
-    var eventText = $("<textarea>")
-        .addClass("description") // col-md
-        .addClass(thisTime)
-        .attr("data-time", time)
-        .val(planner[time]);
-    tdEvent.append(eventText);
-    // eventText.appentTo(tdEvent);
-
-    var tdSubmit = $("<td>").addClass("saveBtn"); // col-md-1
-
-    var icon = $("<i>").addClass("far fa-save fa-2x");
-
-    tdSubmit.append(icon);
-    // icon.appentTo(tdSubmit);
-
-    tr.append(tdTime, tdEvent, tdSubmit);
-
-    $("#myPlanner").append(tr);
-    // tr.appentTo($("#myPlanner"));
-}
-
-function initializePlanner() {
-    var tempPlanner = {};
-
-    for (var i = 8; i < 18; i++) {
-        // tempPlanner.moment(i, "H").format("h a") = "";
-        tempPlanner[moment(i, "H").format("h a")] = "";
+    // Set to "Past" if current row hour is less than this hour
+    if (thisRowHr < currentHour) {
+      thisRow.addClass("past").removeClass("present future");
     }
-    // tempplanner.appendTo($(".container"));
-    return tempPlanner;
+
+    // Set to "Future" if current row hour is greater than this hour
+    if (thisRowHr > currentHour) {
+      thisRow.addClass("future").removeClass("past present");
+    }
+  });
+  };
+
+// Function to load the saved tasks into the planner if they are saved to localStorage
+
+function renderPlanner() {
+
+tasks = localStorage.getItem("tasks");
+tasks = JSON.parse(tasks);
+    
+for (var i = 0; i < tasks.length; i++){
+    var itemHour = tasks[i].hour;
+    var itemText = tasks[i].text; 
+    
+    $("[data-hour=" + itemHour + "]").children("textarea").val(itemText);
 }
+};
 
-$(".saveBtn").on("click", function() {
-    $(this).css("color", "#06AE");
-    var time = $(this)
-        .parent()
-        .find(".description")
-        .attr("data-time");
-    var text = $(this)
-        .parent()
-        .find(".description")
-        .val();
-    console.log(time, text);
+// What will happen on the page load (when the document has loaded)
 
-    planner[time] = text;
+$(document).ready(function(){
+formatRows();
 
-    localStorage.setItem("planner", JSON.stringify(planner));
+if(!localStorage.getItem("tasks")){
+// Run the loadTasks function if there are no tasks in storage so we don't pull localStorage
+loadTasks();
+} 
+// Update the date & time in the header
+$("#currentDay").text(currentDate);
+
+// Render planner from localStorage
+renderPlanner();
+
+// When a save button is clicked, save the task to localStorage
+container.on("click", "button", saveTasks, console.log("Tasks were saved"));
+
 });
-});
 
-$("#clear-schedule").on("click", function(){
+// Function to clear out all the tasks in the planner
+$("#clear-planner").on("click", function() {
     localStorage.clear();
+    tasks = "";
+    location.reload();
+    console.log("Local storage was cleared");
 });
+
+// Function to refresh the page by clicking the refresh button
+
+$("#refresh").on("click", function() {
+    location.reload();
+});
+
+
